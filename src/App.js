88b9e71a83978fd1352057e3b478ae24b473e5e9
Daniel Xiao <danielxiao@Daniels-MacBook-Air.local>
2022-06-11 14:32:44 -0500

    Added in posture detector

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import Video from './Video'
+import Navbar from './Navbar'
+import Info from './Info'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="min-h-screen flex flex-col text-white bg-green-400">
+          <Navbar/>
+			<main className="container mx-auto px-6 pt-16 flex-1 text-center">
+
+				<h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">Welcome to</h2>
+				<h1 className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">Helping Hand</h1>
+
+				<p className="text-base md:text-lg lg:text-2xl mb-20">
+Second Wind is an ecosystem of various software meant to prevent accidental falls, notably in the case of the elderly. We hope you enjoy using our tool!</p>
+        <Video/>
+        </main>
+        <Info/>
+        </div>
   );
 }
 
